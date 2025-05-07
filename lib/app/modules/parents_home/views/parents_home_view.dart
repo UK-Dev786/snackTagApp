638b751449a04a92282b43435afcaed82de20dff
UK-Dev786 <ukdev.codetivelab@gmail.com>
2025-05-07 18:47:13 +0500

    remove the temp debug data

--- lib/app/modules/parents_home/views/parents_home_view.dart
+++ lib/app/modules/parents_home/views/parents_home_view.dart
@@ -88,7 +88,7 @@ class ParentsHomeView extends GetView<ParentsHomeController> {
                             ),
                         itemBuilder: (context, index) {
                           return Container(
-                              height: 110,
+                              height: 95,
                               decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(12),
@@ -273,12 +273,6 @@ class ParentsHomeView extends GetView<ParentsHomeController> {
                                       'Monthly Spending',
                                       style: AppTextStyles.MetropolisMedium
                                           .copyWith(fontSize: 5),
-                                    ),
-                                    // Add debug text to verify data
-                                    Text(
-                                      'ID: ${parentController.childrenList.value[index].childId?.substring(0, 4) ?? "none"}',
-                                      style: TextStyle(
-                                          fontSize: 4, color: Colors.grey),
                                     ),
                                   ],
                                 )
